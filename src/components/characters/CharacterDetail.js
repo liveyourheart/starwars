f2eb73782ab9280ff54c909e92304f570af06d0b
Matt <beepboop@MATTHEWs-MacBook-Pro.local>
2018-09-06 02:03:30 -0500

    updated character components

--- src/components/characters/CharacterDetail.js
+++ src/components/characters/CharacterDetail.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
-import CharacterInfo from './CharacterInfo';
+import React, { Component } from "react";
+import axios from "axios";
+import InfoLabel from "../shared/InfoLabel";
+import RouteLink from "../shared/RouteLink";
 
 export default class CharacterDetail extends Component {
-    render() {
-        const {name, height, mass, hair_color, eye_color, birth_year} = this.props.character;
-        return (
-            <div className='summary'>
-                <CharacterInfo label='Name' data={name} isHeading={true}/>
-                <CharacterInfo label='Birth Year' data={birth_year}/>
-                <CharacterInfo label='Height' data={height}/>
-                <CharacterInfo label='Mass' data={mass}/>
-                <CharacterInfo label='Hair Color' data={hair_color}/>
-                <CharacterInfo label='Eye Color' data={eye_color}/>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      character: {
+        name: "",
+        birth_year: 0,
+        height: 0,
+        mass: 0,
+        hair_color: "",
+        eye_color: "",
+        films: []
+      }
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(`https://swapi.co/api/people/${this.props.match.params.id}`)
+      .then(response => {
+        console.log(response);
+        this.setState({ character: response.data });
+      });
+  }
+  render() {
+    const {
+      name,
+      height,
+      mass,
+      hair_color,
+      eye_color,
+      birth_year,
+      films
+    } = this.state.character;
+    return (
+      <React.Fragment>
+        <div className="summary">
+          <InfoLabel label="Name" data={name} isHeading={true} />
+          <InfoLabel label="Birth Year" data={birth_year} />
+          <InfoLabel label="Height" data={height} />
+          <InfoLabel label="Mass" data={mass} />
+          <InfoLabel label="Hair Color" data={hair_color} />
+          <InfoLabel label="Eye Color" data={eye_color} />
+        </div>
+        <RouteLink text="Back" link="/characters" />
+      </React.Fragment>
+    );
+  }
 }
--- src/components/characters/CharacterSummary.js
+++ src/components/characters/CharacterSummary.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import CharacterInfo from './CharacterInfo';
+import React, { Component } from "react";
+import InfoLabel from "../shared/InfoLabel";
+import { Link } from "react-router-dom";
 
 export default class CharacterSummary extends Component {
-    render() {
-        const {name, height, mass, hair_color, eye_color, birth_year} = this.props.character;
-        return (
-            <div className='summary'>
-                <CharacterInfo label='Name' data={name} isHeading={true}/>
-                <CharacterInfo label='Birth Year' data={birth_year}/>
-                <CharacterInfo label='Height' data={height}/>
-                <CharacterInfo label='Mass' data={mass}/>
-                <CharacterInfo label='Hair Color' data={hair_color}/>
-                <CharacterInfo label='Eye Color' data={eye_color}/>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.state = {
+      id: this.props.character.url
+        .split("https://swapi.co/api/people/")
+        .join("")
+    };
+  }
+
+  render() {
+    const {
+      name,
+      height,
+      mass,
+      hair_color,
+      eye_color,
+      birth_year,
+      url
+    } = this.props.character;
+    return (
+      <Link to={`/characters/${this.state.id}`}>
+        <div className="summary">
+          <InfoLabel label="Name" data={name} isHeading={true} />
+          <InfoLabel label="Birth Year" data={birth_year} />
+          <InfoLabel label="Height" data={height} />
+          <InfoLabel label="Mass" data={mass} />
+          <InfoLabel label="Hair Color" data={hair_color} />
+          <InfoLabel label="Eye Color" data={eye_color} />
+        </div>
+      </Link>
+    );
+  }
 }
