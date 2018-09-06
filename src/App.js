10c257fce90b712b4e726403b6a300f1ebce1f84
Matt <beepboop@MATTHEWs-MacBook-Pro.local>
2018-09-06 02:04:46 -0500

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import CharacterList from './components/characters/CharacterList';
-import './App.css';
+import React, { Component } from "react";
+import CharacterList from "./components/characters/CharacterList";
+import CharacterDetail from "./components/characters/CharacterDetail";
+import Home from "./components/home/Home";
+import FilmList from "./components/films/FilmList";
+import Header from "./components/shared/Header";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
-  
   render() {
     return (
-      <CharacterList />
+      <React.Fragment>
+        <Router>
+          <React.Fragment>
+            <div className="header">
+              <Header />
+            </div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/characters" component={CharacterList} />
+            <Route exact path="/characters/:id" component={CharacterDetail} />
+            <Route exact path="/films" component={FilmList} />
+          </React.Fragment>
+        </Router>
+      </React.Fragment>
     );
   }
 }
